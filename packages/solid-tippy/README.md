854b1c920d4c28d1428996e278220ebc1d5e7a08
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-11-14 16:16:47 +0800

    init

---
+++ packages/solid-tippy/README.md
@@ -0,0 +1,63 @@
+# solid-tippy
+
+> SolidJS bindings for Tippy.js
+
+[![NPM](https://img.shields.io/npm/v/solid-tippy.svg)](https://www.npmjs.com/package/solid-tippy) [![JavaScript Style Guide](https://badgen.net/badge/code%20style/airbnb/ff5a5f?icon=airbnb)](https://github.com/airbnb/javascript)[![Open in CodeSandbox](https://img.shields.io/badge/Open%20in-CodeSandbox-blue?style=flat-square&logo=codesandbox)](https://codesandbox.io/s/github/LXSMNSYC/solid-tippy/tree/main/examples/solid-tippy-demo)
+
+## Install
+
+```bash
+npm i tippy.js solid-tippy
+```
+
+```bash
+yarn add tippy.js solid-tippy
+```
+
+```bash
+pnpm add tippy.js solid-tippy
+```
+
+## Usage
+
+```jsx
+import { tippy, useTippy } from 'solid-tippy';
+
+// As directive
+<button
+  use:tippy={{
+    props: {
+      content: 'This is a tooltip.',
+    },
+  }}
+>
+  Example
+</button>
+
+// as hook
+const [anchor, setAnchor] = createSignal();
+
+useTippy(anchor, {
+  props: {
+    content: 'This is a tooltip.',
+  },
+});
+
+<button ref={anchor}>
+  Example
+</button>
+```
+
+### Options
+
+- `disabled`: Disables the Tippy instance. Defaults to `false`.
+- `hidden`: Hides the Tippy instance. Defaults to `false`.
+- `props`: Props to be passed to the Tippy instance when creating or updating it. You can read more about it [here](https://atomiks.github.io/tippyjs/v6/all-props/).
+
+### Headless mode
+
+You may use `tippyHeadless` or `useTippyHeadless` instead of `tippy` and `useTippy` for Headless Tippy instances.
+
+## License
+
+MIT © [lxsmnsyc](https://github.com/lxsmnsyc)
---
+++ packages/solid-tippy/src/index.ts
@@ -0,0 +1,167 @@
+import {
+  createComputed,
+  createEffect,
+  createSignal,
+  onCleanup,
+  untrack,
+} from 'solid-js';
+import makeTippy, { Instance, Props } from 'tippy.js';
+import makeHeadlessTippy from 'tippy.js/headless';
+
+export interface TippyOptions {
+  disabled?: boolean;
+  hidden?: boolean;
+  props?: Partial<Props>;
+}
+
+export function tippy<T extends Element>(
+  target: T,
+  options?: TippyOptions,
+): void {
+  createEffect(() => {
+    const instance = makeTippy(target, untrack(() => options?.props));
+
+    createComputed(() => {
+      if (options?.disabled) {
+        instance.disable();
+      } else {
+        instance.enable();
+      }
+    });
+
+    createComputed(() => {
+      if (options?.hidden) {
+        instance.hide();
+      } else {
+        instance.show();
+      }
+    });
+
+    createComputed(() => {
+      instance.setProps({
+        ...(options?.props ?? {}),
+      });
+    });
+
+    onCleanup(() => {
+      instance.destroy();
+    });
+  });
+}
+
+export function tippyHeadless<T extends Element>(
+  target: T,
+  options?: TippyOptions,
+): void {
+  createEffect(() => {
+    const instance = makeHeadlessTippy(target, untrack(() => options?.props));
+
+    createComputed(() => {
+      if (options?.disabled) {
+        instance.disable();
+      } else {
+        instance.enable();
+      }
+    });
+
+    createComputed(() => {
+      if (options?.hidden) {
+        instance.hide();
+      } else {
+        instance.show();
+      }
+    });
+
+    createComputed(() => {
+      instance.setProps({
+        ...(options?.props ?? {}),
+      });
+    });
+
+    onCleanup(() => {
+      instance.destroy();
+    });
+  });
+}
+
+export function useTippy<T extends Element>(
+  target: () => T,
+  options?: TippyOptions,
+): () => Instance | undefined {
+  const [current, setCurrent] = createSignal<Instance>();
+
+  createEffect(() => {
+    const instance = makeTippy(target(), untrack(() => options?.props));
+
+    setCurrent(instance);
+
+    createComputed(() => {
+      if (options?.disabled) {
+        instance.disable();
+      } else {
+        instance.enable();
+      }
+    });
+
+    createComputed(() => {
+      if (options?.hidden) {
+        instance.hide();
+      } else {
+        instance.show();
+      }
+    });
+
+    createComputed(() => {
+      instance.setProps({
+        ...(options?.props ?? {}),
+      });
+    });
+
+    onCleanup(() => {
+      instance.destroy();
+    });
+  });
+
+  return () => current();
+}
+
+export function useTippyHeadless<T extends Element>(
+  target: () => T,
+  options?: TippyOptions,
+): () => Instance | undefined {
+  const [current, setCurrent] = createSignal<Instance>();
+
+  createEffect(() => {
+    const instance = makeHeadlessTippy(target(), untrack(() => options?.props));
+
+    setCurrent(instance);
+
+    createComputed(() => {
+      if (options?.disabled) {
+        instance.disable();
+      } else {
+        instance.enable();
+      }
+    });
+
+    createComputed(() => {
+      if (options?.hidden) {
+        instance.hide();
+      } else {
+        instance.show();
+      }
+    });
+
+    createComputed(() => {
+      instance.setProps({
+        ...(options?.props ?? {}),
+      });
+    });
+
+    onCleanup(() => {
+      instance.destroy();
+    });
+  });
+
+  return () => current();
+}
